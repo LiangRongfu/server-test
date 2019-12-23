86210b0ff6b549f69b98876a088303d0943a3f21
IMRofu <1009224151@qq.com>
2019-12-23 20:38:24 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const router = new Router();
+const bodyparser = require('koa-bodyparser');
+
+const app = new Koa();
+const cors = require('koa2-cors');
+
+app.use(cors());
+app.use(bodyparser());
+
+const info = require('./info.json');
+router.get('/api/info', async ctx => {
+  ctx.body = {
+    code: 0,
+    data: info
+  }
+});
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(8090);

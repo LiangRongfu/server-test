3e449f731ca1c2cfdcfa46a3eca226d42ff0317f
IMRofu <1009224151@qq.com>
2019-12-23 20:10:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 let http = require('http');
+let info = require('./info.json');
 let server = http.createServer(function(req, res){
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('2019blog');
+  res.setHeader('Content-Type', 'application/json');
+  res.end(info);
 });
 server.listen(3000, ()=>{
   console.log('success');
